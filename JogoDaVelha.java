2de0a89e383c54c7138976d7303263dd7784355e
gustavo-cordova <gugugoedert@gmail.com>
2025-07-10 23:24:53 +0000

    fim de jogo e tratar os dados

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -3,18 +3,25 @@ import java.util.Scanner;
 public class JogoDaVelha {
     public static void main(String[] args) {
         Scanner elementoDigitado = new Scanner(System.in);
-        System.out.println("Digite X ou O:");
-        String entrada = elementoDigitado.nextLine();
-        int posI = elementoDigitado.nextInt();
-        int posJ = elementoDigitado.nextInt();
+        System.out.println("Digite X ou O, seguido da linha e da coluna. Ex: x(2,1):");
+        
 
         String[][] elementos = {{" ", " "," "},
                                 {" ", " "," "},
                                 {" ", " "," "}};
-        elementos[posI][posJ] = entrada;
-
+        
+        while (!JogoDaVelha.FimDeJogo(elementos)){
+        String entrada = elementoDigitado.nextLine();
+        JogoDaVelha.TrataOsDados(entrada);
+        int posI = Integer.parseInt(JogoDaVelha.TrataOsDados(entrada)[1])-1;
+        int posJ =  Integer.parseInt(JogoDaVelha.TrataOsDados(entrada)[2])-1;
+        String letra = JogoDaVelha.TrataOsDados(entrada)[0];
+        if (JogoDaVelha.Verificador(elementos,posI,posJ)){
+            elementos[posI][posJ] = letra;
+        }
         JogoDaVelha.MostraElementosNoConsole(elementos);
-        JogoDaVelha.Verificador(elementos, posI,posJ);
+        }
+        
 
         elementoDigitado.close();
     }
@@ -42,5 +49,42 @@ public class JogoDaVelha {
             return false;
         }
         else return true;
-    }      
+    }
+
+    public static String[] TrataOsDados(String entrada) {
+        entrada = entrada.trim();
+        String letra = entrada.substring(0,1);
+        String posI = entrada.substring(2, 3);
+        String posJ = entrada.substring(4, 5);
+        String[] dadosFormatados = {letra,posI,posJ};
+        return dadosFormatados;
+    }
+
+    //ganhou ou não ganhou
+    public static boolean FimDeJogo(String[][] elementos) {
+
+        for (int z=0;z<3;z++) {
+            if (elementos[z][0].equals(elementos[z][1]) && elementos[z][1].equals(elementos[z][2]) && (elementos[z][0].equalsIgnoreCase("X") || elementos[z][0].equalsIgnoreCase("O"))) {
+            return true;
+            }
+        }
+
+        for (int z=0;z<3;z++) {
+            if (elementos[0][z].equals(elementos[1][z]) && elementos[1][z].equals(elementos[2][z]) && (elementos[0][z].equalsIgnoreCase("X") || elementos[0][z].equalsIgnoreCase("O"))) {
+            return true;
+            }
+        }
+        
+            if (elementos[0][0].equals(elementos[1][1]) && elementos[1][1].equals(elementos[2][2]) && (elementos[0][0].equalsIgnoreCase("X") || elementos[0][0].equalsIgnoreCase("O"))) {
+                return true;
+        
+            }
+
+            if (elementos[0][2].equals(elementos[1][1]) && elementos[1][1].equals(elementos[2][0]) && (elementos[0][2].equalsIgnoreCase("X") || elementos[0][2].equalsIgnoreCase("O"))) {
+                return true;
+            }
+
+            return false;
+      
+}
 }
